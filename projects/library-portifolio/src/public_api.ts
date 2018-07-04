48cf30823e7ded5df2122c0217ef5fd17f164d06
bruno <bfurlaneto@gmail.com>
2018-07-03 22:06:05 -0300

    delete repositorio

--- projects/library-portifolio/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of library-portifolio
- */
-
-export * from './lib/library-portifolio.service';
-export * from './lib/library-portifolio.component';
-export * from './lib/library-portifolio.module';
