a441ff7e5bb0afc5584cda1bad69c5b64af95f39
frontendbuilders <frontendbuilders@gmail.com>
2018-07-08 21:29:00 -0300

    comitando estruturas iniciais do layout da aplicação

--- projects/games-rank/src/app/app.module.ts
+++ projects/games-rank/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './layout/principal/principal.component';
+import { MenuPrincipalComponent } from './layout/menu-principal/menu-principal.component';
+import { RodapeComponent } from './layout/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent,
+    MenuPrincipalComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
